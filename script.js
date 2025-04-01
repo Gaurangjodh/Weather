96b176c4dc50991d62122ed1ed9067e70c489dc5
Gaurang Jodh <104861437+Gaurangjodh@users.noreply.github.com>
2025-04-01 18:50:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,20 +26,27 @@ async function getWeather() {
 }
 function changeBackground(condition) {
     console.log('Weather condition:', condition); // Log the condition
+    const bgIcon = document.querySelector('.bg-icon');
+    bgIcon.classList.remove('rain', 'clouds', 'sun', 'snow', 'default');
     if (condition.includes('rain')) {
         console.log('Changing background to rainy'); // Log change
         document.body.style.background = "linear-gradient(90deg, rgba(0,2,84,1) 12%, rgba(151,213,255,1) 89%)";
-    } else if (condition.includes('cloud')) {
+        bgIcon.classList.add('rain')
+    } else if (condition.includes('cloud') || condition.includes('mist')) {
         console.log('Changing background to cloudy');
         document.body.style.background = "linear-gradient(90deg, rgba(38,39,73,1) 12%, rgba(114,140,157,1) 89%)";
-    } else if (condition.includes('sunny') || condition.includes('Clear')) {
+        bgIcon.classList.add('clouds')
+    } else if (condition.includes('sunny') || condition.includes('clear')) {
         console.log('Changing background to sunny');
         document.body.style.background = "linear-gradient(90deg, rgba(255,156,0,1) 12%, rgba(255,251,151,1) 89%)";
+        bgIcon.classList.add('sun')
     } else if (condition.includes('snow')) {
         console.log('Changing background to snowy');
         document.body.style.background = "linear-gradient(90deg, rgba(69,149,161,1) 12%, rgba(255,255,255,1) 89%)";
+        bgIcon.classList.add('snow')
     } else {
         console.log('Changing background to default');
         document.body.style.background = "linear-gradient(to right, #4facfe, #00f2fe)"; // Default gradient
+        bgIcon.classList.add('default')
     }
 }
